7b662d03915d373b48baaccf03679784fddfcb6b
Manuel Rosario <manurosario31@gmail.com>
2025-06-10 20:38:53 -0400

    Frontend y Backend terminados - ui con responsive y conexion del front con el back

--- user-card/src/App.jsx
+++ user-card/src/App.jsx
@@ -2,9 +2,10 @@
 import { FiPhone } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
 import { FiMapPin } from "react-icons/fi";
+import { FiMap } from "react-icons/fi";
 import {  useState, useEffect } from 'react'
 
-const endpoint =  'https://3447-2001-1308-2414-1c00-1cc-9cf-9293-9e57.ngrok-free.app/api'
+const endpoint = 'http://localhost:3005/api'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         }
       })
       const data = await response.json();
+
       setContactos(data.info);
 
     } catch (err) {
@@ -29,16 +31,17 @@ function App() {
   } , [])
 
   return (
-    <main className='flex flex-col items-center p-0 sm:p-0 md:p-0 lg:p-0 xl:p-0'>
-      <h1 className='font-bold text-3xl'>Mis Contactos</h1>
-      <section className='w-[70%] grid items-center justify-center grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-10'>
+    <main className='flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-3xl mt-16'>Mis Contactos</h1>
+      <section className='w-full grid justify-items-center grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-16 gap-6 px-10'>
         {contactos.map(contacto => (
-          <article key={contacto.id} className='bg-white border-black text-black p-4 w-110 rounded-2xl'>
-            <h2 className='font-bold text-3xl'>{`${contacto.nombre} ${contacto.apellido}`}</h2>
-            <p className='flex items-center gap-2 mt-2'><FiMail size={'1.5em'}/> {contacto.correo}</p>
-            <p className='flex items-center gap-2 text-neutral-800 mt-2
-            '><FiMapPin size={'1.5em'}/>{`${contacto.direccion}`} <span className='text-neutral-600 font-bold'>{`${contacto.ciudad}, ${contacto.pais}`}</span></p>
-            <p className='flex items-center gap-2 mt-2' ><FiPhone size={'1.5em'}/>{contacto.telefono}</p>
+          <article key={contacto.id} className='bg-white border-black text-black p-8 w-full rounded-2xl'>
+            <h2 className='font-bold text-3xl mb-4'>{`${contacto.nombre} ${contacto.apellido}`}</h2>
+            <p className='flex items-center gap-2 text-neutral-800 mt-2 text-lg
+            '><FiMapPin size={'1.5em'}/>{`${contacto.direccion}`}</p>
+            <p className='flex items-center gap-2 mt-3 text-lg'><FiMap size={'1.5em'}/>{`${contacto.ciudad}, ${contacto.pais}`}</p>
+            <p className='flex items-center gap-2 mt-3 text-lg'><FiMail size={'1.5em'}/> {contacto.correo}</p>
+            <p className='flex items-center gap-2 mt-3 text-lg' ><FiPhone size={'1.5em'}/>{contacto.telefono}</p>
           </article>
         ))}
       </section>
